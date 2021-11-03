1ad8a1304f61e32fa15e030d2d03c29c0a94582c
camyk991 <kurczakpee@gmail.com>
2021-11-03 11:03:29 +0100

    loadMore button

--- src/components/ChatRoom.js
+++ src/components/ChatRoom.js
@@ -1,4 +1,4 @@
-import {collection, orderBy, limit, onSnapshot, getFirestore, addDoc, serverTimestamp, query } from 'firebase/firestore';
+import {collection, orderBy, limit, onSnapshot, getFirestore, addDoc, serverTimestamp, query, startAfter } from 'firebase/firestore';
 import React, {useState, useEffect, useRef} from 'react';
 import {getAuth } from "firebase/auth";
 import ChatMessage from './ChatMessage';
@@ -6,52 +6,62 @@ import ChatMessage from './ChatMessage';
 
 export default function ChatRoom() {
 
+    const LOAD_MORE_NUM = 5;
+
     const db = getFirestore();
     const auth = getAuth();
 
     const [messages, setMessages] = useState([]);
     const [formValue, setFormValue] = useState('');
-  
+    const [all, setIfAll] = useState(false);
+    const [limitValue, setLimitValue] = useState(25);
+    let limitVal = 5;
+
+
     const goDown = useRef()
   
-  
     useEffect(() => {
-      const q = query(collection(db, "messages"), orderBy("createdAt", "desc"), limit(25));
-  
-      const unsub = onSnapshot(q, (doc) => {
-        console.log(doc.docs);
-        setMessages(doc.docs.reverse().map(d => ({...d.data(), id: d.id})));
-      });
-      return unsub;
-    }, [])
-  
+        let q = query(collection(db, "messages"), orderBy("createdAt", "desc"), limit(limitValue));
+
+        const unsub = onSnapshot(q, (doc) => {
+          doc.docs.length + 1 > limitVal ? setIfAll(false) : setIfAll(true);
+          setMessages(doc.docs.reverse().map(d => ({...d.data(), id: d.id})));
+        });
+
+        return unsub;
+    }, [limitValue])
+
+    
     useEffect(() => {
-      goDown.current.scrollIntoView({behavior: 'smooth'});
+      // goDown.current.scrollIntoView({behavior: 'smooth'});
+      
     }, [messages])
   
-  const sendMessage = async(e) => {
-    e.preventDefault();
-  
-    const {uid, photoURL} = auth.currentUser;
-  
-    await addDoc(collection(db, "messages"), {
-      "text": formValue,
-      "createdAt": serverTimestamp(),
-      uid,
-      photoURL
-    });
-  
-    setFormValue('');
-  
-    goDown.current.scrollIntoView({behavior: 'smooth'});
-  }
+    const sendMessage = async(e) => {
+      e.preventDefault();
+    
+      const {uid, photoURL} = auth.currentUser;
+    
+      await addDoc(collection(db, "messages"), {
+        "text": formValue,
+        "createdAt": serverTimestamp(),
+        uid,
+        photoURL
+      });
+    
+      setFormValue('');
+    
+      goDown.current.scrollIntoView({behavior: 'smooth'});
+    }
   
     return (
       <>
         <main>
-          {messages && messages.map((el) => 
-            <ChatMessage text={el.text} key={el.id} uid={el.uid} photoURL={el.photoURL} />
-          )}
+
+            <button className={all ? 'no-more' : 'more'} onClick={() => setLimitValue(limitValue + 5)}>load</button>
+            {messages && messages.map((el) => 
+                <ChatMessage text={el.text} key={el.id} uid={el.uid} photoURL={el.photoURL} />
+            )}
   
           <div ref={goDown}></div>
         </main>
